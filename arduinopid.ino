951a619632b41b617ce15c959f656cb5a8bad366
perrin <pwaldock@gmail.com>
2020-09-24 18:17:11 -0700

    changing to be used with teensy 3.2

--- arduinopid.ino
+++ arduinopid.ino
@@ -1,161 +1,190 @@
-#include <string.h>
+/*
+ * This is PID feedback control code intended to be used with the Teensy 3.2: 
+ * https://www.pjrc.com/teensy/teensy31.html
+ * Its pins are here:
+ * https://www.pjrc.com/teensy/pinout.html
+ * And its schematic is here:
+ * https://www.pjrc.com/teensy/schematic.html
+ * 
+ * Most of the PID feedback control code is from here: https://github.com/mike-matera/FastPID/blob/master/src/FastPID.cpp
+ */
 
-#define OUTPUT_PIN 5	//PWM output on pin #5 because it has fastest PWM cycle
-#define INPUT_PIN A0	//PWM input on analog 0 because it is an analog pin
 
-#define MAX_OUTPUT_VOLTAGE 5 	//maximum board output voltage
-#define PWM_BITS 8				//bit resolution of PWM
-#define REFERENCE_VOLTAGE 1.1	//ADC peak reference voltage
-#define ADC_BITS 10				//bit resolution of ADC
+#include <FastPID.h>	//prepackaged arduino feedback control library
 
-#define BITSHIFT_BITS 6			//shift values left by 6 bits to approximate floating point numbers
-#define OUTPUT_BITSHIFT 2		//divide output by 2^OUTPUT_BITSHIFT
+//defines the input and output pins 
+#define PIN_INPUT     A0
+#define PIN_OUTPUT    A14
 
-#define MAX_ITERM 1000			//maximum value of the integral term (to stop integral wind-up)
-#define READ_AVERAGES 8			//analog inputs get read 8 times
+//sets the max PWM and ADC resolutions for the Teensy 3.2
+#define REFERENCE_VOLTAGE 3.3	//can change to 1.2 volts if we want
+#define PWM_BITS 16
+#define ADC_BITS 12
+#define SIGNED_OUTPUT false		//output only goes from 0 to +
+#define SAMPLE_PERIOD_US 50		//sets the PID loop frequency (too low and the code doesn't work
 
+const uint16_t SAMPLE_RATE_HZ = 1000000/SAMPLE_PERIOD_US;
+
+#define READ_AVERAGES 64			//analog inputs get averaged over this many times
+
+//conversion macros
+#define int2volts(x, b) REFERENCE_VOLTAGE*x/(1 << (b - 1))
+#define volts2int(x, b) (1 << (b - 1))*x/REFERENCE_VOLTAGE
+
+//sets communication parameters
 #define SERIAL_BAUD 9600
 #define INSTRING_LENGTH 80
 
+//if defined, then the microcontroller times how long it takes to do a loop
+//undefine for speed
+#define TIME_FEEDBACK_LOOP
 
-//note: could be better to use this: https://playground.arduino.cc/Code/PIDLibraryAdaptiveTuningsExample/
+//pid feedback control parameters
+float kp=10, ki=200, kd=0;
+uint16_t setpoint = volts2int(0.7, ADC_BITS); //default setpoint
 
-uint16_t minPWM;	//minimum output
-uint16_t maxPWM;	//maximum output
-uint16_t setpoint;	//desired setpoint
-
-//PID constants(divide by 2^BITSHIFT_BITS to get actual number)
-uint16_t kp = 64;
-uint16_t ki = 1;
-uint16_t kd = 0;
-
-//integral term
-uint32_t iterm = 0;
-//last measured value
-uint16_t lasterror = 0;
-
+//things that only need to be defined if feedback control is active
 #ifdef SERIAL_BAUD
-char instring[INSTRING_LENGTH];
-uint8_t inind = 0;
+	#define READ_PERIOD_MS 100			//how quickly it prints
+	char instring[INSTRING_LENGTH];		//input string
+	uint8_t inind = 0;
+	bool serialActive = false;			//serial active flag
+	bool printout = false;				//if true, print feedback stats
+	volatile bool readSerial = true;	//read serial flag
 #endif
 
-//utility functions
-uint8_t volts2pwm(float voltage);
-uint16_t volts2bsint(float voltage);
-float bsint2volts(uint16_t integer);
-uint16_t output2pwm(uint32_t output);
-uint16_t averagedread();
-void updateparams(char* string);
+volatile bool pidcalc = true; //flag saying to perform feedback control calc
+
+IntervalTimer pidTimer;	//used for timing the pid feedback loop
+IntervalTimer readTimer; //used for timing the serial read code
+
+FastPID myPID(kp, ki, kd, SAMPLE_RATE_HZ, PWM_BITS, SIGNED_OUTPUT); //feedback control object
 
 
-void setup() {
-	// put your setup code here, to run once:
+void updateparams(char* string);	//updates parameters given string
+uint32_t averagedread();			//reads and averages data
+void setpidcalc();					//sets pid calc flag (ISR)
+void setreadserial();				//sets read serial flag (ISR)
 
-#ifdef SERIAL_BAUD
-	Serial.begin(SERIAL_BAUD);
-	Serial.println("Serial Active");
-#endif
+void setup()
+{
+	//sets pin states
+	pinMode(PIN_INPUT, INPUT);
+	pinMode(PIN_OUTPUT, OUTPUT);
+	analogReadResolution(ADC_BITS);
+	analogWriteResolution(PWM_BITS); 
 
-	//set the reference voltage
-	if (REFERENCE_VOLTAGE == 1.1)
+	//sets reference voltage
+	if (REFERENCE_VOLTAGE == 1.2)
 	{
 		//could also use EXTERNAL and use an external reference voltage
-		analogReference(INTERNAL);	//sets reference voltage to 1.1
+		analogReference(INTERNAL);	//sets reference voltage to 1.2
+	}
+	else
+	{
+		analogReference(DEFAULT); //3.3v
 	}
 
-	//set limits
-	minPWM = volts2pwm(0.5);		//don't output less than .5 volts
-	maxPWM = volts2pwm(1.3);		//don't output more than 1.3 volts
-	setpoint = volts2bsint(.05);	//want to read .05 volts on the ADC
-
+//sets up serial
 #ifdef SERIAL_BAUD
-	Serial.println("PID Active");
+	Serial.begin(SERIAL_BAUD);
+	serialActive = true;
+	if (myPID.err()) {
+		Serial.println("There is a configuration error!");
+		for (;;) {}
+	}
+	else
+	{
+		Serial.println("PID Begin");
+	}
+	readTimer.begin(setreadserial, READ_PERIOD_MS*1000);
 #endif
 
+	pidTimer.begin(setpidcalc, SAMPLE_PERIOD_US);
 }
 
-void loop() {
+//feedback control variables 
+uint32_t feedback = 0;
+uint16_t out = 0;
+#ifdef TIME_FEEDBACK_LOOP
+uint32_t ts = 0;
+uint32_t tss = 0;
+#endif
 
-#ifdef SERIAL_BAUD
-	while (Serial.available() > 0)
+//main loop
+void loop()
+{	
+	//run feedback control loop
+	if(pidcalc)
 	{
-		instring[inind] = Serial.read();
-		//Serial.print(instring[inind], HEX);
-		if(inind >= INSTRING_LENGTH)
+#ifdef TIME_FEEDBACK_LOOP
+		ts = micros();
+#endif
+		feedback = analogRead(PIN_INPUT);
+		out = myPID.step(2*setpoint, feedback); //not sure why but you have to multiply the setpoint by 2
+		analogWrite(PIN_OUTPUT, out);
+		pidcalc = false;
+#ifdef TIME_FEEDBACK_LOOP
+		tss = micros();
+#endif
+	}
+#ifdef SERIAL_BAUD
+	//if Serial connection exists and it's time to check for serial
+	if(Serial && readSerial)
+	{
+		//sets up serial connection if necessary
+		if(!serialActive)
 		{
-			Serial.println("Incoming String Too Long");
-			inind = 0;
+			Serial.begin(SERIAL_BAUD);
+			serialActive = true;
 		}
-		else if (instring[inind] == '\n')
+		//reads data from user and possibly sets parameters using it
+		while (Serial.available() > 0)
 		{
-			instring[inind] = "\0";
-			updateparams(instring);
-			inind = 0;
+			instring[inind] = Serial.read();
+			if(inind >= INSTRING_LENGTH)
+			{
+				Serial.println("Incoming String Too Long");
+				inind = 0;
+			}
+			else if (instring[inind] == '\n')
+			{
+				instring[inind] = '\0';
+				updateparams(instring);
+				inind = 0;
+			}
+			else
+			{
+				inind++;
+			}
 		}
-		else
+		//prints parameters
+		if(printout)
 		{
-			inind++;
+			Serial.print("s:");
+			Serial.print(setpoint);
+			Serial.print("\t f:");
+			Serial.print(feedback);
+			Serial.print("\t o:");
+#ifdef TIME_FEEDBACK_LOOP
+			Serial.print(out);
+			Serial.print("\t t:");
+			Serial.println(tss - ts);
+#else
+			Serial.println(out);
+#endif
 		}
+		readSerial = false;
+	}
+	else if(serialActive) //if no serial object but it thinks the connection is active make sure to end the connection
+	{
+		Serial.end();
+		serialActive = false;
 	}
 #endif
-	
-	// put your main code here, to run repeatedly:
-	uint16_t measure = averagedread();		//measure from ADC
-	int16_t error = setpoint - measure;
-	//PID calculations
-	int32_t dterm = kd*(error - lasterror);
-	int32_t pterm = kp*error;
-	iterm += ki*error;
-	uint32_t output = dterm + pterm + iterm;
-	
-	uint16_t pwm = output2pwm(output);
-	analogWrite(OUTPUT_PIN, pwm);
-
-	lasterror = error;
 }
 
-uint8_t volts2pwm(float voltage)
-{
-	return voltage*(pow(2,PWM_BITS) - 1)/MAX_OUTPUT_VOLTAGE;
-}
-
-uint16_t volts2bsint(float voltage)
-{
-	return (uint16_t)(voltage*(pow(2,ADC_BITS) - 1)/REFERENCE_VOLTAGE) << BITSHIFT_BITS;
-}
-
-float bsint2volts(uint16_t integer)
-{
-	return (integer*REFERENCE_VOLTAGE/(pow(2,BITSHIFT_BITS + ADC_BITS)));
-}
-
-uint16_t averagedread()
-{
-	uint32_t total = 0;
-	for (uint8_t i = 0; i < READ_AVERAGES; i++)
-	{
-		total += analogRead(INPUT_PIN);
-	}
-
-	return (total << BITSHIFT_BITS)/READ_AVERAGES;
-}
-
-uint16_t output2pwm(uint32_t output)
-{
-	uint16_t pwm = output >> (BITSHIFT_BITS + OUTPUT_BITSHIFT);
-	if (pwm > maxPWM)
-	{
-		pwm = maxPWM;
-	}
-	else if (pwm < minPWM)
-	{
-		pwm = minPWM;
-	}
-
-	return pwm;
-		
-}
-
+//updates pid feedback parameters given a string from the user
 void updateparams(char* string)
 {
 	char* peql = strchr(string, '=');	//find address of equals sign in string
@@ -165,28 +194,43 @@ void updateparams(char* string)
 		
 		if(!strcmp(string, "kp"))
 		{
-			kp = atoi(&(string[3]));
+			kp = atof(&(string[3]));
 			Serial.print("kp=");
 			Serial.println(kp);
 		}
 		else if(!strcmp(string, "ki"))
 		{
-			ki = atoi(&(string[3]));
+			ki = atof(&(string[3]));
 			Serial.print("ki=");
 			Serial.println(ki);
 		}
 		else if(!strcmp(string, "kd"))
 		{
-			kd = atoi(&(string[3]));
+			kd = atof(&(string[3]));
 			Serial.print("kd=");
 			Serial.println(kd);
 		}
 		else if(!strcmp(string, "sp"))
 		{
-			setpoint = atoi(&(string[3]));
+			setpoint = atof(&(string[3]));
 			Serial.print("setpoint=");
 			Serial.println(setpoint);
 		}
+#ifdef SERIAL_BAUD
+		else if(!strcmp(string, "po"))
+		{
+			printout = atoi(&(string[3]));
+			Serial.print("printout=");
+			Serial.println(printout);
+		}
+#endif
+		else
+		{
+			Serial.println("Invalid Command");
+		}
+
+		myPID.setCoefficients(kp, ki, kd, SAMPLE_RATE_HZ);
+		
 	}
 	else
 	{
@@ -212,10 +256,39 @@ void updateparams(char* string)
 			else if(!strcmp(string, "sp"))
 			{
 				Serial.print("setpoint=");
-				Serial.print(bsint2volts(setpoint));
+				Serial.print(int2volts(setpoint, ADC_BITS));
 				Serial.print("V, ");
 				Serial.println(setpoint);
+			}
+			else
+			{
+				Serial.println("Invalid Command");
 			}
 		}
 	}
 }
+
+//conducts an averaged read
+uint32_t averagedread()
+{
+	uint32_t total = 0;
+	for (uint8_t i = 0; i < READ_AVERAGES; i++)
+	{
+		total += analogRead(PIN_INPUT);
+	}
+
+	return total/READ_AVERAGES;
+}
+
+//ISRs that set flags
+void setpidcalc()
+{
+	pidcalc = true;
+}
+
+#ifdef SERIAL_BAUD
+void setreadserial()
+{
+	readSerial = true;
+}
+#endif
