899e8830ffb5a609708087b565e0ba115e72e249
PerrinWaldock <PerrinWaldock@users.noreply.github.com>
2025-02-01 13:37:35 -0800

    switched to different way of launching application

--- gui/App/main.qml
+++ gui/App/main.qml
@@ -1,13 +1,31 @@
 import QtQuick
+import QtQuick.Layouts
+import QtQuick.Controls
+import QtQuick.Window
+import QtQuick.Controls.Material
+import io.qt.textproperties
 
-Rectangle {
-    id: main
-    width: 200
-    height: 200
-    color: "green"
+ApplicationWindow {
+    id: page
+    width: 800
+    height: 400
+    visible: true
+    Material.theme: Material.Light
+    Material.accent: Material.Red
 
-    Text {
-        text: "Hello World"
-        anchors.centerIn: main
+    Bridge {
+        id: bridge
+    }
+
+    Rectangle {
+        id: main
+        width: 200
+        height: 200
+        color: "green"
+
+        Text {
+            text: "Hello World"
+            anchors.centerIn: main
+        }
     }
 }
--- gui/main.py
+++ gui/main.py
@@ -1,16 +1,19 @@
 import sys
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtQuick import QQuickView
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQuickControls2 import QQuickStyle
+
+from bridge import Bridge
 
 if __name__ == "__main__":
-    app = QGuiApplication()
-    view = QQuickView()
-    view.engine().addImportPath(sys.path[0])
-    view.loadFromModule("App", "Main")
-    view.setResizeMode(QQuickView.SizeRootObjectToView) 
-    view.show()
-    ex = app.exec()
-    del view
-    sys.exit(ex)
-    
-    
+    app = QGuiApplication(sys.argv)
+    # QQuickStyle.setStyle("Material")
+    engine = QQmlApplicationEngine()
+    engine.addImportPath(sys.path[0])
+    engine.loadFromModule("App", "Main")
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
